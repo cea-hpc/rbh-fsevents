c7a57e1ed05f24c917cebfdbdd95275efe7d6c42
Quentin Bouget <ypsah@devyard.org>
2021-06-29 01:33:58 +0200

    yaml: implement xattr fsevent emission
    Signed-off-by: Quentin Bouget <ypsah@devyard.org>
    Change-Id: I6b7454084f41b8d98460ac33454a59f69501b20b

--- src/serialization.c
+++ src/serialization.c
@@ -2517,6 +2517,14 @@ parse_delete(yaml_parser_t *parser, struct rbh_fsevent *delete)
 #define NS_XATTR_TAG "!ns_xattr"
 
 static bool
+emit_ns_xattr(yaml_emitter_t *emitter __attribute__((unused)),
+              const struct rbh_fsevent *ns_xattr __attribute__((unused)))
+{
+    error(EXIT_FAILURE, ENOSYS, __func__);
+    __builtin_unreachable();
+}
+
+static bool
 parse_ns_xattr(yaml_parser_t *parser __attribute__((unused)),
                struct rbh_fsevent *ns_xattr __attribute__((unused)))
 {
@@ -2531,6 +2539,14 @@ parse_ns_xattr(yaml_parser_t *parser __attribute__((unused)),
 #define INODE_XATTR_TAG "!inode_xattr"
 
 static bool
+emit_inode_xattr(yaml_emitter_t *emitter __attribute__((unused)),
+                 const struct rbh_fsevent *inode_xattr __attribute__((unused)))
+{
+    error(EXIT_FAILURE, ENOSYS, __func__);
+    __builtin_unreachable();
+}
+
+static bool
 parse_inode_xattr(yaml_parser_t *parser __attribute__((unused)),
                   struct rbh_fsevent *inode_xattr __attribute__((unused)))
 {
@@ -2541,11 +2557,15 @@ parse_inode_xattr(yaml_parser_t *parser __attribute__((unused)),
 /*---------------------------------- xattr -----------------------------------*/
 
 static bool
-emit_xattr(yaml_emitter_t *emitter __attribute__((unused)),
-           const struct rbh_fsevent *xattr __attribute__((unused)))
+emit_xattr(yaml_emitter_t *emitter, const struct rbh_fsevent *xattr)
 {
-    error(EXIT_FAILURE, ENOSYS, __func__);
-    __builtin_unreachable();
+    if (xattr->ns.parent_id) {
+        assert(xattr->ns.parent_id);
+        return emit_ns_xattr(emitter, xattr);
+    }
+    assert(xattr->ns.parent_id == NULL);
+
+    return emit_inode_xattr(emitter, xattr);
 }
 
 /*----------------------------------------------------------------------------*
