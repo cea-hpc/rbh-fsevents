86dea8c78111302a16babe6dce918ab778a3f197
Quentin Bouget <ypsah@devyard.org>
2021-07-10 22:43:45 +0200

    deduplicator: batch fsevents from the source
    Can't deduplicate fsevents if we only ever see one at a time.
    
    Signed-off-by: Quentin Bouget <ypsah@devyard.org>
    Change-Id: Ie507d57dce5435a989d798472a563fb2109a5063

--- rbh-fsevents.c
+++ rbh-fsevents.c
@@ -205,7 +205,8 @@ records2fsevents(struct rbh_iterator *records)
 
 /*---------------------------------- feed() ----------------------------------*/
 
-static const size_t BATCH_SIZE = 1;
+ // TODO: tune this finely or make it configurable
+static const size_t BATCH_SIZE = 1 << 10;
 
 static void
 feed(struct sink *sink, struct source *source)
