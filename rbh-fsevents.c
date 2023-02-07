b3a19501fb836c62615d1f8aa8319c2b5ad8849f
Sebastien Gougeaud <sebastien.gougeaud@cea.fr>
2023-02-07 15:16:25 +0100

    enricher: add lustre enricher
    Change-Id: Ie4cb0152a2b55ec769952da6ecc59f1f05030b2c
    Signed-off-by: Sebastien Gougeaud <sebastien.gougeaud@cea.fr>

--- rbh-fsevents.c
+++ rbh-fsevents.c
@@ -34,8 +34,12 @@ enum rbh_source_t {
 static void
 usage(void)
 {
+    /* TODO: remove the enricher option to have a Robinhood URI as enrich
+     * mountpoint. */
+    /* TODO: accept source as a URI or a '-', like src:lustre:lustre-MDT0000. */
     const char *message =
-        "usage: %s [-h] [--raw] [--enrich MOUNTPOINT] [--lustre] SOURCE DESTINATION\n"
+        "usage: %s [-h] [--raw] [[--enricher ENRICHER] --enrich MOUNTPOINT] [--lustre]\n"
+        "          SOURCE DESTINATION\n"
         "\n"
         "Collect changelog records from SOURCE, optionally enrich them with data\n"
         "collected from MOUNTPOINT and send them to DESTINATION.\n"
@@ -53,6 +57,8 @@ usage(void)
         "    -r, --raw       do not enrich changelog records (default)\n"
         "    -e, --enrich MOUNTPOINT\n"
         "                    enrich changelog records by querying MOUNTPOINT as needed\n"
+        "    -E, --enricher ENRICHER\n"
+        "                    use ENRICHER enricher (default is posix)\n"
         "    -l, --lustre    consider SOURCE is an MDT name\n"
         "\n"
         "Note that uploading raw records to a RobinHood backend will fail, they have to\n"
@@ -222,6 +228,11 @@ main(int argc, char *argv[])
             .val = 'e',
         },
         {
+            .name = "enricher",
+            .has_arg = required_argument,
+            .val = 'E',
+        },
+        {
             .name = "help",
             .val = 'h',
         },
@@ -239,12 +250,15 @@ main(int argc, char *argv[])
     char c;
 
     /* Parse the command line */
-    while ((c = getopt_long(argc, argv, "e:hlr", LONG_OPTIONS, NULL)) != -1) {
+    while ((c = getopt_long(argc, argv, "e:E:hlr", LONG_OPTIONS, NULL)) != -1) {
         switch (c) {
         case 'e':
             mount_fd = open(optarg, O_RDONLY | O_CLOEXEC);
             if (mount_fd == -1)
                 error(EXIT_FAILURE, errno, "open: %s", optarg);
+            break;
+        case 'E':
+            parse_enricher_type(optarg);
             break;
         case 'h':
             usage();
