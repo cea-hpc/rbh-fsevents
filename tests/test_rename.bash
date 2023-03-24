f30b68855cb249a2359dc6b0d8dfcb9c7fac8edd
VALERI Yoann <yoann.valeri@cea.fr>
2023-03-24 14:02:53 +0100

    tests: add RENAME tests with overwriten files
    Change-Id: Idf078b8d3d5a6257713c586a61091958fb708105
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- tests/test_rename.bash
+++ tests/test_rename.bash
@@ -17,12 +17,12 @@ test_rename()
 {
     local entry="$1"
     local entry_renamed="$2"
+    local count=$3
 
     rbh_fsevents --enrich "$LUSTRE_DIR" --lustre "$LUSTRE_MDT" \
         "rbh:mongo:$testdb"
 
     local entries=$(mongo "$testdb" --eval "db.entries.find()" | wc -l)
-    local count=$(find . | wc -l)
     if [[ $entries -ne $count ]]; then
         error "There should be only $count entries in the database"
     fi
@@ -59,7 +59,8 @@ test_rename_same_dir()
     touch $entry
     mv $entry $entry_renamed
 
-    test_rename "$entry" "$entry_renamed"
+    local count=$(find . | wc -l)
+    test_rename "$entry" "$entry_renamed" $count
 }
 
 test_rename_different_dir()
@@ -70,14 +71,46 @@ test_rename_different_dir()
     touch $entry
     mv $entry tmp/$entry_renamed
 
-    test_rename "$entry" "$entry_renamed"
+    local count=$(find . | wc -l)
+    test_rename "$entry" "$entry_renamed" $count
+}
+
+test_rename_overwrite_data()
+{
+    local entry="test_entry"
+    local entry_renamed="test_entry_renamed"
+    touch $entry
+    mkdir tmp
+    touch tmp/$entry_renamed
+
+    rbh_fsevents --enrich "$LUSTRE_DIR" --lustre "$LUSTRE_MDT" \
+        "rbh:mongo:$testdb"
+
+    # Retrieve the overwriten link's id to test it was deleted after test_rename
+    local old_entry=$(mongo "$testdb" --eval \
+                      'db.entries.find({"ns.name":"'$entry_renamed'"},
+                                       {"_id": 1})')
+
+    old_entry="BinData${old_entry#*BinData}"
+    old_entry="${old_entry%)*})"
+
+    local count=$(find . | wc -l)
+
+    mv $entry tmp/$entry_renamed
+
+    test_rename "$entry" "$entry_renamed" $count
+
+    # Check the overwriten entry is still in the DB but has no link anymore
+    find_attribute '"_id":'$old_entry \
+                   '"ns": { $exists : true }' '"ns": { $size : 0 }'
 }
 
 ################################################################################
 #                                     MAIN                                     #
 ################################################################################
 
-declare -a tests=(test_rename_same_dir test_rename_different_dir)
+declare -a tests=(test_rename_same_dir test_rename_different_dir
+                  test_rename_overwrite_data)
 
 LUSTRE_DIR=/mnt/lustre/
 cd "$LUSTRE_DIR"
