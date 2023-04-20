776bc9e7a4c130f9fea310fc3a42ebccef54b56f
VALERI Yoann <yoann.valeri@cea.fr>
2023-04-20 15:06:42 +0200

    tests: add acceptance test for all events
    This patch adds a test that will trigger all events at once in a
    changelog and check they are all correctly recorded in the resulting
    database.
    
    Change-Id: I0a42b0a7bd6e05b7e660e467ca6bfc71f59644dc
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- tests/test_hsm.bash
+++ tests/test_hsm.bash
@@ -30,7 +30,7 @@ test_hsm()
     local entry="$1"
     local state="$2"
 
-    rbh_fsevents --enrich "$LUSTRE_DIR" --lustre "$LUSTRE_MDT" \
+    rbh_fsevents --enrich rbh:lustre:"$LUSTRE_DIR" --lustre "$LUSTRE_MDT" \
         "rbh:mongo:$testdb"
 
     # Since an archived copy of $entry still exists, the DB should contain two
--- tests/test_rename.bash
+++ tests/test_rename.bash
@@ -19,7 +19,7 @@ test_rename()
     local entry_renamed="$2"
     local count=$3
 
-    rbh_fsevents --enrich "$LUSTRE_DIR" --lustre "$LUSTRE_MDT" \
+    rbh_fsevents --enrich rbh:lustre:"$LUSTRE_DIR" --lustre "$LUSTRE_MDT" \
         "rbh:mongo:$testdb"
 
     local entries=$(mongo "$testdb" --eval "db.entries.find()" | wc -l)
@@ -83,7 +83,7 @@ test_rename_overwrite_data()
     mkdir tmp
     touch tmp/$entry_renamed
 
-    rbh_fsevents --enrich "$LUSTRE_DIR" --lustre "$LUSTRE_MDT" \
+    rbh_fsevents --enrich rbh:lustre:"$LUSTRE_DIR" --lustre "$LUSTRE_MDT" \
         "rbh:mongo:$testdb"
 
     mv $entry tmp/$entry_renamed
@@ -100,7 +100,7 @@ test_rename_overwrite_data_with_hsm_copy()
     mkdir tmp
     touch tmp/$entry_renamed
 
-    rbh_fsevents --enrich "$LUSTRE_DIR" --lustre "$LUSTRE_MDT" \
+    rbh_fsevents --enrich rbh:lustre:"$LUSTRE_DIR" --lustre "$LUSTRE_MDT" \
         "rbh:mongo:$testdb"
 
     hsm_archive_file tmp/$entry_renamed
