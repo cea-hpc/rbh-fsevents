df299793c0d4b7d89414c616a4d77903f50bd968
VALERI Yoann <yoann.valeri@cea.fr>
2023-05-03 14:44:24 +0200

    tests: add verify_statx function
    Add a function that will, given a file, verify its state in the database
    reflects its statx values as given back by the filesystem.
    
    Also change some small things requested in different patches of the
    stack.
    
    Change-Id: I4c2097f62471b6b9b30258a454cc3de30c10e979
    Signed-off-by: VALERI Yoann <yoann.valeri@cea.fr>

--- tests/test_setxattr.bash
+++ tests/test_setxattr.bash
@@ -15,71 +15,61 @@ test_dir=$(dirname $(readlink -e $0))
 
 test_setxattr()
 {
-    touch "test_file"
+    local entry="test_file"
+    touch "$entry"
 
     invoke_rbh-fsevents
 
     clear_changelogs
-    setfattr -n user.test -v 42 "test_file"
+    setfattr -n user.test -v 42 "$entry"
 
     invoke_rbh-fsevents
 
-    mongo $testdb --eval "db.entries.find()"
-
-    find_attribute '"ns.name":"test_file"'
-    # to uncomment once the path is enriched
-    # find_attribute "\"ns.xattrs.path\":\"/${testdir#*lustre/}/test_file\""
-    find_attribute '"xattrs.user":{$exists: true}' '"ns.name":"test_file"'
-    find_attribute '"xattrs.user.test":{$exists: true}' '"ns.name":"test_file"'
-    find_attribute '"statx.ctime.sec":NumberLong('$(statx +%Z "test_file")')' \
-                   '"ns.name":"test_file"'
-    find_attribute '"statx.ctime.nsec":0' '"ns.name":"test_file"'
+    verify_statx $entry
+    find_attribute '"xattrs.user":{$exists: true}' '"ns.name":"'$entry'"'
+    find_attribute '"xattrs.user.test":{$exists: true}' '"ns.name":"'$entry'"'
 }
 
 test_setxattr_remove()
 {
-    touch "test_file"
+    local entry="test_file"
+    touch "$entry"
 
     invoke_rbh-fsevents
 
     clear_changelogs
-    setfattr -n user.test -v 42 "test_file"
+    setfattr -n user.test -v 42 "$entry"
 
     invoke_rbh-fsevents
 
-    find_attribute '"ns.name":"test_file"'
-    # to uncomment once the path is enriched
-    # find_attribute "\"ns.xattrs.path\":\"/${testdir#*lustre/}/test_file\""
+    verify_statx $entry
     find_attribute '"xattrs.user":{$exists: true}' '"ns.name":"test_file"'
     find_attribute '"xattrs.user.test":{$exists: true}' '"ns.name":"test_file"'
 
     clear_changelogs
-    setfattr --remove="user.test" "test_file"
+    setfattr --remove="user.test" "$entry"
 
     invoke_rbh-fsevents
 
-    find_attribute '"ns.name":"test_file"'
-    # to uncomment once the path is enriched
-    # find_attribute "\"ns.xattrs.path\":\"/${testdir#*lustre/}/test_file\""
+    verify_statx $entry
     find_attribute '"xattrs.user":{$exists: true}' '"ns.name":"test_file"'
     find_attribute '"xattrs.user.test":{$exists: false}' '"ns.name":"test_file"'
 }
 
 test_check_last_setxattr_is_inserted()
 {
-    touch "test_file"
+    local entry="test_file"
+    touch "$entry"
 
     invoke_rbh-fsevents
 
     clear_changelogs
-    setfattr -n user.test -v 42 "test_file"
-    setfattr -n user.blob -v 43 "test_file"
+    setfattr -n user.test -v 42 "$entry"
+    setfattr -n user.blob -v 43 "$entry"
 
     invoke_rbh-fsevents
 
-    find_attribute '"ns.name":"test_file"'
-    # to uncomment once the path is enriched
-    # find_attribute "\"ns.xattrs.path\":\"/${testdir#*lustre/}/test_file\""
+    verify_statx $entry
     find_attribute '"xattrs.user":{$exists: true}' '"ns.name":"test_file"'
     find_attribute '"xattrs.user.test":{$exists: true}' '"ns.name":"test_file"'
     find_attribute '"xattrs.user.blob":{$exists: true}' '"ns.name":"test_file"'
@@ -87,12 +77,13 @@ test_check_last_setxattr_is_inserted()
 
 test_setxattr_replace()
 {
-    touch "test_file"
+    local entry="test_file"
+    touch "$entry"
 
     invoke_rbh-fsevents
 
     clear_changelogs
-    setfattr -n user.test -v 42 "test_file"
+    setfattr -n user.test -v 42 "$entry"
 
     invoke_rbh-fsevents
 
